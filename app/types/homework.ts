b17066de4e5b7bd4fa5ded21e8ed5700c6a93190
retro <sangachoi2103@gmail.com>
2020-08-12 23:29:28 +0900

    2020-08-12 editAssign 버그 수정 및 SubAssign 수정 구현

--- app/types/homework.ts
+++ app/types/homework.ts
@@ -1,7 +1,11 @@
+import { concat, now } from 'lodash';
+
+import { current } from 'immer';
+
 export interface AssignListType {
   assigns: Array<AssignType>;
 }
-export interface AssignType {
+export class AssignType {
   id: string;
   title: string;
   desc: string;
@@ -10,9 +14,53 @@ export interface AssignType {
   isCompleted: boolean;
   status: number;
   subAssigns: Array<SubAssignType>;
+  
+  // print: () => string;
+
+  constructor(title: string, desc: string, due: Date) {
+    this.title = title;
+    this.desc = desc;
+    this.due = due;
+    this.out = new Date(Date.now());
+    this.isCompleted = false;
+    this.status = 0;
+    this.subAssigns = [];
+    this.id = '000';
+  //   this.print = () => {
+  //     let info = `제목: ${this.title}\n설명: ${this.desc}\nDUE: ${
+  //       this.due
+  //     }\nOUT: ${this.out}\nisCompleted?: ${this.isCompleted}\nstatus: ${
+  //       this.status * 100
+  //     }% 완료\n`;
+  //     let callBack: (
+  //       pv: string,
+  //       cur: SubAssignType,
+  //       index: number,
+  //       arr: Array<SubAssignType>,
+  //     ) => string = function (pv, cur, index, arr) {
+  //       if (index == 0) return pv + cur.print();
+  //       else return callBack(pv + cur.print(), arr[index + 1], index + 1, arr);
+  //     };
+  //     let subAssigns: string =
+  //       'subAssigns: \n' + this.subAssigns.reduce(callBack, '') + '\n';
+  //     return info + subAssigns;
+  //   };
+  }
 }
-export interface SubAssignType {
+
+export class SubAssignType {
   text: string;
   isCompleted: boolean;
   id: string;
+  // print: () => string;
+
+  constructor(text: string) {
+    this.text = text;
+    this.isCompleted = false;
+    this.id = '000';
+    // this.print = () => {
+    //   let isCompleted = this.isCompleted ? 'completed' : 'incompleted';
+    //   return `${this.text}: ${isCompleted}\n`;
+    // };
+  }
 }
