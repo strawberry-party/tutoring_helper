f097584e6928a36b604815cbee56f94693976272
TaeHyung Kim <tylerkim129@gmail.com>
2020-08-17 16:17:21 +0900

    2020-08-17 redux적용 전 UI 수정 버전

---
+++ app/component/headerOptions.js
@@ -0,0 +1,12 @@
+const headerOptions = {
+  title: '',
+  headerTintColor: 'white',
+  headerStyle: {
+    backgroundColor: '#e91e63',
+  },
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+}
+
+export default headerOptions;
