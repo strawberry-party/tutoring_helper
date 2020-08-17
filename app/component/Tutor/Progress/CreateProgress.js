97152e74126ccfa8eca08b82987aedeea667694b
TaeHyung Kim <tylerkim129@gmail.com>
2020-08-17 17:54:40 +0900

    2020-08-17 merge 전 임시 commit

---
+++ app/component/Tutor/Progress/CreateProgress.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Input } from 'react-native-elements';
+
+function CreateProgress(props) {
+  return (
+    <View>
+      <Input placeholder='hello' />
+    </View>
+  );
+}
+
+export default CreateProgress;
